d62be1c7ac911b142a310744c17fdb5a8720c219
vikneshvicky <vikneshkc2014@gmail.com>
2016-10-21 11:55:29 +0530

    Update and rename .gitignore to oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("Enter the number to find even or odd");
+scanf("%d",&n);
+if(n%2==0)
+printf("The number is Even");
+else
+printf("THE number is Odd");
+getch();
+}
