8b9c471c1c9bc4a8e79eff75a6c74a1190888f55
vikneshvicky <vikneshkc2014@gmail.com>
2016-10-24 10:18:47 +0530

    Create positiveornegative.c

---
+++ positiveornegative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int number;
+printf("Enter a number:");
+scanf("%d",&number);
+if (number<=0)
+{
+if (number == 0.0)
+printf("You entered 0.");
+else
+printf("You entered a negative number.");
+}
+else
+printf("You entered a positive number.");
+getch();
+}
